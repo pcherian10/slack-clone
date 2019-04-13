bfe4065d071d6330815c8aa594350bc2b421ae91
pcherian10 <pcherian10@gmail.com>
2019-04-13 17:17:25 -0400

    added routes, integrating firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+
+  var config = {
+    apiKey: "AIzaSyDQ8PyUdkx6JpYqj30QvvvOwY2xPoHobig",
+    authDomain: "pc-slack-clone.firebaseapp.com",
+    databaseURL: "https://pc-slack-clone.firebaseio.com",
+    projectId: "pc-slack-clone",
+    storageBucket: "pc-slack-clone.appspot.com",
+    messagingSenderId: "410911827163"
+  };
+firebase.initializeApp(config);
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+        </Switch>
+    </Router>
+)
+
+
+ReactDOM.render(<Root />, 
+    document.getElementById('root'));
 registerServiceWorker();
